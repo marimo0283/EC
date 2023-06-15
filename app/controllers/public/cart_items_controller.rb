3b6bc8586fce375f4efa6f29fe9ab7afc857e508
marimo0283 <kugelschreiber1002@gmail.com>
2023-06-15 08:45:24 +0000

    アソシエーション追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,23 +1,36 @@
 class Public::CartItemsController < ApplicationController
   def index
-    
+    @total_price = my_cart.total_price
+    @items = my_cart.items
   end
 
   def create
+    if my_cart.add_item(product_id: params[:product_id], quantity: params[:quantity])
+      redirect_to cart_items_path
+    else
+      redirect_to cart_items_path
+    end
   end
 
   def update
-
+    if my_cart.update_item(product_id: params[:product_id], quantity: params[:quantity])
+      redirect_to cart_items_path
+    else
+      redirect_to cart_items_path
+    end
   end
 
   def destroy
-  end
-
-  def destroy_all
+    if my_cart.delete_item(product_id: params[:product_id])
+      redirect_to cart_items_path
+    else
+      redirect_to cart_items_path
+    end
   end
 
   private
-  def genre_params
-    params.require(:cart_item).permit(:order_id, :item_id, :amount)
+
+  def my_cart
+    Cart.find(params[:cart_id])
   end
 end
