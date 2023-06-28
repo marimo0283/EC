09409bc3091c0a734fd451a48489316ef5520355
marimo0283 <kugelschreiber1002@gmail.com>
2023-06-28 06:12:15 +0000

    [Add]会員編集画面、会員情報更新

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -16,4 +16,10 @@ class Admin::CustomersController < ApplicationController
     @customer.update(customer_params)
     redirect_to admin_customers_path
   end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :last_name_kana, :first_name, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
+  end
+
 end
