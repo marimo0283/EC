686e96820ee14e22b333f66186fc2869857ebafb
marimo0283 <kugelschreiber1002@gmail.com>
2023-07-06 05:40:17 +0000

    注文機能準備

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,20 @@
 class Public::OrdersController < ApplicationController
+  def index
+
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def comfirm
+  end
+
+  def create
+  end
+
+  def complete
+  end
 end
