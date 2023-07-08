79a0833d3f0424653f7d5d239208529e953b2f3c
marimo0283 <kugelschreiber1002@gmail.com>
2023-07-08 08:43:42 +0000

    [Update]税込金額・小計表示、カートアイテムページ調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   has_one_attached :item_image
   has_many :cart_items, dependent: :destroy
   belongs_to :genre
+
+  ## 消費税を求めるメソッド
+  def with_tax_price
+      (price * 1.1).floor
+  end
 end
